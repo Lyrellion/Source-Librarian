3e02ee994c7a6e1e9a71f3bff8b6bdef794030c4
Jarva <4622609+Jarva@users.noreply.github.com>
2024-07-15 13:25:31 +0100

    fix: update spelling mistakes

--- src/features/helpers/mana.ts
+++ src/features/helpers/mana.ts
@@ -11,7 +11,7 @@ export const mana = {
             "- Enchant everything with Mana Boost and Mana Regen. Each level of Mana Boost will give you 25 mana. Each level of Mana Regen will give you 2 mana per second.",
             "- Use an Amulet of Mana Boost or Amulet of Mana Regen. These provide 50 mana, and 3 mana regen respectively.",
             "- Craft two Rings of Greater Discount, each of these reduce your spell cost by 20 mana.",
-            "- Upgrade your Spellbook to the highest tier you can. Each book tier grants you 50 mana and 1 mana regen.",
+            "- Upgrade your Spell Book to the highest tier you can. Each book tier grants you 50 mana and 1 mana regen.",
             "- Put a Magic Capacity thread in the highest level slot that you can. This increases your max mana by 10% per level."
         ]
 
--- src/features/helpers/wiki.ts
+++ src/features/helpers/wiki.ts
@@ -11,7 +11,7 @@ export const wiki = {
             .setURL("https://www.arsnouveau.wiki/")
             .setDescription("The Ars Nouveau wiki is auto generated from the patchouli book available in-game. The wiki should always follow the latest released Ars Nouveau version.")
             .addFields(
-                { name: 'Why does my book open the wiki?' , value: "You're mising the [Patchouli](https://www.curseforge.com/minecraft/mc-mods/patchouli) mod, once installed you will be able to browse the book in-game." },
+                { name: 'Why does my book open the wiki?' , value: "You're missing the [Patchouli](https://www.curseforge.com/minecraft/mc-mods/patchouli) mod, once installed you will be able to browse the book in-game." },
                 { name: 'Noticed a problem?', value: "Please raise an issue on the [GitHub Repository](https://github.com/Sarenor/arsnouveau-wiki/issues)" },
             );
 
